49591e9746f8c1a4edc22bfda18f39272de6191d
johirulalam <johirulalam365@gmail.com>
2023-03-05 09:48:37 +0600

    install log and added winston console log

--- user_service/app.js
+++ user_service/app.js
@@ -7,12 +7,47 @@ import userRoutes from "./routes/userRoutes";
 import models from "./models";
 import connectWithDb from "./mongodb";
 import { handleErrors } from "./middlewares/handleErrors";
+import winston from "winston";
+import expressWinston from "express-winston";
+import winstonFile from "winston-daily-rotate-file";
+import winstonMongo from "winston-mongodb";
+import {ElasticsearchTransport} from "winston-elasticsearch";
 
 const app = express();
 app.use(bodyParser.json());
+
+const processRequest = async(req, res, next)=>{
+    let correlationId = req.headers['x-correlation-id'];
+    if(!correlationId){
+        correlationId = Date.now().toString();
+        req.headers['x-correlation-id'] = correlationId;
+    }
+    res.set('x-correlation-id', correlationId);
+    return next();
+}
+
+app.use(processRequest);
 const log = (msg) => console.log(msg);
 
 connectWithDb();
+const getMessage = (req,res)=>{
+    let obj = {
+        correlationId: req.headers['x-correlation-id'],
+        requestBody: req.body
+    }
+
+    return JSON.stringify(obj);
+}
+const infoLogger = expressWinston.logger({
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+    meta:true,
+    msg: getMessage
+})
+app.use(infoLogger);
+
 app.use(authRoutes);
 app.use(adminUserRoutes);
 app.use(userRoutes);
